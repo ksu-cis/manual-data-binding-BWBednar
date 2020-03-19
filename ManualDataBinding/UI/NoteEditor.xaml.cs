ba1f463065e8fce499b62262a1c515fbfb4edf86
Brandon Bednar <bwbednar@ksu.edu>
2020-03-19 13:22:59 -0500

    Online Lecture Work for Manual Data Binding

--- ManualDataBinding/UI/NoteEditor.xaml.cs
+++ ManualDataBinding/UI/NoteEditor.xaml.cs
@@ -19,9 +19,61 @@ namespace ManualDataBinding.UI
     /// </summary>
     public partial class NoteEditor : UserControl
     {
+        private Note note;
+        /// <summary>
+        /// Note that will be edited by this control
+        /// </summary>
+        public Note Note 
+        {
+            get { return note; } 
+            set
+            {
+                if (note == value) return;
+                if (note != null) note.NoteChanged -= OnNoteChange;
+                note = value;
+                if (note != null)
+                {
+                    note.NoteChanged += OnNoteChange;
+                    OnNoteChange(note, new EventArgs());
+                }
+            }        
+        }
+
         public NoteEditor()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Event handler for when the Note changes
+        /// </summary>
+        /// <param name="sender">The Note instance that is changing</param>
+        /// <param name="e">The event arguments describing the event</param>
+        public void OnNoteChange(object sender, EventArgs e)
+        {
+            if (Note == null) return;
+            Title.Text = Note.Title;
+            Body.Text = Note.Body;            
+        }
+
+        /// <summary>
+        /// Event handler for when the title changes
+        /// </summary>
+        /// <param name="sender">The Textbox that changed</param>
+        /// <param name="e">The event args</param>
+        public void OnTitleChanged(object sender, TextChangedEventArgs e)
+        {
+            Note.Title = Title.Text;
+        }
+
+        /// <summary>
+        /// Event handler for when the body changes
+        /// </summary>
+        /// <param name="sender">The textbox that changed</param>
+        /// <param name="e">The event args</param>
+        public void OnBodyChanged(object sender, TextChangedEventArgs e)
+        {
+            Note.Body = Body.Text;
         }
     }
 }
